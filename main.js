a7d95e2daeebe7d51d01c04f897e3287407da7fe
shaunhailey <ediblelysol@gmail.com>
2017-07-16 17:44:33 -0400

    html css done javascript incomplete

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
 //main.js
+let workingArray = []
+
+document.getElementById("1").addEventListener("click", function() {
+  workingArray.push(1)
+  }
+
+document.getElementById("2").addEventListener("click", function() {
+  workingArray.push(2)
+}
+
+document.getElementById("3").addEventListener("click", function() {
+  workingArray.push(3)
+}
+
+document.getElementById("4").addEventListener("click", function() {
+  workingArray.push(4)
+}
+
+document.getElementById("5").addEventListener("click", function() {
+  workingArray.push(5)
+}
+
+document.getElementById("6").addEventListener("click", function() {
+  workingArray.push(6)
+}
+
+document.getElementById("7").addEventListener("click", function() {
+  workingArray.push(7)
+}
+
+document.getElementById("8").addEventListener("click", function() {
+  workingArray.push(8)
+}
+
+document.getElementById("9").addEventListener("click", function() {
+  workingArray.push(9)
+}
+
+document.getElementById("0").addEventListener("click", function() {
+  workingArray.push(0)
+}
+
+document.getElementById("X").addEventListener("click", function() {
+  workingArray.push(X)
+}
+
+document.getElementById("/").addEventListener("click", function() {
+  workingArray.push(/)
+}
+
+document.getElementById("+").addEventListener("click", function() {
+  workingArray.push(+)
+}
+
+document.getElementById("-").addEventListener("click", function() {
+  workingArray.push(-)
+}
+
+document.getElementById("=").addEventListener("click", function() {
+}
+
+document.getElementById("C").addEventListener("click", function() {
+}
